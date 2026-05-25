6abec3e3e14a2feb28dfe6a75f37256dc1725c5c
ktk-07 <ktkofficial01@gmail.com>
2026-05-25 22:00:30 +0800

    Fixed GQA, need to fix naming convention

--- model/llama.py
+++ model/llama.py
@@ -141,40 +141,46 @@ class MultiheadSelfAttention(nn.Module):
         return mhsa_output, attn_weights
 
 # GQA:Training Generalized Multi-Query Transformer Models from Multi-Head Checkpoints https://arxiv.org/pdf/2305.13245
-# Each qroup of query heads is attached to 1 set of K and V heads
+# 2 main idea to this GQA paper.
+# 1. Converting MHA into GQA, which is called uptraining
+# 2. Introduces Grouped-query attention which divides query heads into G groups, where each G groups shares 1 single k and v head. GQA-1 == MQA
 class GroupQueryAttention(nn.Module):
-    def __init__(self, h, g, d_model):
+    def __init__(self, max_seq_len, h, g, d_model):
         super().__init__()
         assert d_model % h == 0, "Embedding Dimension must be divisible by H"
-        assert h % g == 0, "Total Number of heads must be divisible by number of heads in group"
+        assert h % g == 0, "Total Number of heads must be divisible by number of group"
         self.h = h # Total no of heads
-        self.g = g # Group Size aka no of heads in the group
-        self.head_dim = d_model // self.h
-        self.num_of_groups = self.g // self.h
+        self.g = g # Number of groups
+        self.head_dim = d_model // self.h # Head Dimension
+        self.num_of_heads = self.h // self.g # Number of heads in a group
         self.linear_q = nn.Linear(in_features=d_model,out_features=d_model, bias=False)
-        self.linear_k = nn.Linear(in_features=d_model,out_features=self.head_dim * self.num_of_groups, bias=False)
-        self.linear_v = nn.Linear(in_features=d_model,out_features=self.head_dim * self.num_of_groups bias=False)
-        self.linear_o = nn.Linear(in_features=self.head_dim * self.num_of_groups,out_features=d_model, bias=False)
+        self.linear_k = nn.Linear(in_features=d_model,out_features=self.head_dim * self.g, bias=False)
+        self.linear_v = nn.Linear(in_features=d_model,out_features=self.head_dim * self.g, bias=False)
+        self.linear_o = nn.Linear(in_features=d_model,out_features=d_model, bias=False)
         self.rope = RotaryPositionEmbedding(max_seq_len=max_seq_len, head_dim=self.head_dim, base=10000)
 
     def forward(self, x : torch.Tensor, mask : torch.Tensor = None):
         # input : batch x max_seq_len x d_model
+        B, max_seq_len, d_model = x.shape
         Q = self.linear_q(x)
-        Q = Q.reshape(B, max_seq_len, self.num_of_groups, self.head_dim * self.g).permute(0,2,1,3) # batch_size x num_of_groups x max_seq_len x self.head_dim * self.g
-        
-        K = self.linear_k(x) # batch x max_seq_len x head_dim
-        K = K.unsqueeze(0).permute(0,2,1,3) # batch_size x num_of_groups x max_seq_len x self.head_dim
-        K = K.repeat(1,1,1,self.g) # batch_size x 1 x max_seq_len x self.head_dim * self.g
+        Q = Q.reshape(B, max_seq_len, self.h, self.head_dim).permute(0,2,1,3) # batch_size x num_of_heas x max_seq_len x self.head_dim
+        # The reason why we do this is because we need each query head to be independent
 
+        K = self.linear_k(x) # batch x max_seq_len x head_dim
+        K = K.reshape(B, max_seq_len, self.g, self.head_dim).permute(0,2,1,3) # batch_size x num_of_groups x max_seq_len x self.head_dim
 
         # Apply RoPE to Q and K
         Q = self.rope(Q)
         K = self.rope(K)
 
         V = self.linear_v(x) # batch x max_seq_len x head_dim
-        V = V.unsqueeze(0).permute(0,2,1,3) # batch_size x num_of_groups x max_seq_len x self.head_dim
-        V = V.repeat(1,1,1,self.g) # batch_size x 1 x max_seq_len x self.head_dim * self.g
+        V = V.reshape(B, max_seq_len, self.g, self.head_dim).permute(0,2,1,3) # batch_size x num_of_groups x max_seq_len x self.head_dim
 
+        # Important to use repeat_interleave instead of repeat to match group strucutre
+        K = K.repeat_interleave(self.num_of_heads,dim=1) # B x self.h x max_seq_len x self.head_dim
+        V = V.repeat_interleave(self.num_of_heads,dim=1) # B x self.h x max_seq_len x self.head_dim
+
+        # input B x self.h x max_seq_len x self.head_dim
         attn_score, attn_weights = scaled_dot_product_attention(Q=Q, K=K, V=V, mask=mask)
         attn_score_reshaped = attn_score.permute(0,2,1,3).reshape(B, max_seq_len, d_model)
         mhsa_output = self.linear_o(attn_score_reshaped)
