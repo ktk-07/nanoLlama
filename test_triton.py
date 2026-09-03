a576209558dcd8c2c9760d3915ae1280a6710bbf
ktk-07 <ktkofficial01@gmail.com>
2026-09-04 01:13:26 +0800

    Implemented the safesoftmax accurately

--- test_triton.py
+++ test_triton.py
@@ -27,7 +27,6 @@ from torch.utils._triton import has_triton
 # 5. Implement Flash Attn Backward
 
 # Phase 2, Just make the different functions work on arbitary dimension
-
 if not has_triton:
     print("Skipping because triton is not installed in this machine")
 else:
@@ -46,50 +45,55 @@ else:
                                    n_elements,
                                    BLOCK_SIZE
                                    ):
-        pid0 = tl.progam_id(axis=0)
-        pid1 = tl.progam_id(axis=1)
+        pid0 = tl.program_id(axis=0)
+        pid1 = tl.program_id(axis=1)
 
-        row_idx = pid0 * stride1 + pid1
-        row_start = row_idx * dimensions[reduction_axis]
-        # Running statistics to keep track off
-        prev_max = 0
-        running_max = float("-inf")
-        running_denominator = 0.0
+        row_idx = pid0 * dimensions[1] + pid1
+        row_start = row_idx * dimensions[2]
 
+        # 3 Loops
+        # 1 Loop for the global_max in that dimension
+        # 1 Loop for denominator
+        # 1 Loop for the computation
+        
         # 1 program loads 1 row
         # This codeblock here is sequential (similar to python' range) but using the tl.range lets trition
         # → Triton runtime/compiler IR loop
         # → good when loop bounds depend on runtime values
         # → exposes loop optimization controls to Triton 
-        for i in tl.range(0, dimensions[reduction_axis], BLOCK_SIZE):
-            block_start = i + row_start
-            offsets = tl.arange(block_start, block_start + BLOCK_SIZE)
+
+        global_max = float("-inf") # unless there is another way to represent it
+        for i in tl.range(0, dimensions[reduction_axis], BLOCK_SIZE): 
+            cols = i + tl.arange(0, BLOCK_SIZE)
             # Create a mask to guard memory operations against out-of-bounds accesses.
-            mask = offsets < n_elements
+            mask = cols < dimensions[reduction_axis]
+            offsets = row_start + cols
+            # Load x from DRAM, masking out any extra elements in case the input is not a multiple of the block size.
+            cur_block_vals = tl.load(x_ptr + offsets,mask=mask, other=-float('inf'))
+            local_max = tl.max(cur_block_vals, axis=0)
+            global_max = tl.maximum(global_max,local_max)
+
+        denominator = 0.0
+        for i in tl.range(0, dimensions[reduction_axis], BLOCK_SIZE):
+            cols = i + tl.arange(0, BLOCK_SIZE)
+            mask = cols < dimensions[reduction_axis]
+            offsets = row_start + cols
             # Load x from DRAM, masking out any extra elements in case the input is not a multiple of the block size.
             cur_block_vals = tl.load(x_ptr + offsets, mask=mask, other=-float('inf'))
-
-            local_max = tl.max(cur_block_vals, axis=0)
-
-            # Then this should be sequential already no?
-            running_max = max(local_max,running_max)
-            running_denominator *= tl.exp(prev_max - running_max)
-            running_denominator += tl.exp(local_max - running_max)
-            prev_max = running_max
+            local_sum = tl_sum(tl.exp(cur_block_vals - global_max), axis=0)
+            denominator += local_sum
 
         # We will run the loop 1 more time to compute the actual softmaxed value
         for i in tl.range(0, dimensions[reduction_axis], BLOCK_SIZE):
-            block_start = i+program_idx
-            offsets = tl.arange(block_start, block_start + BLOCK_SIZE)
-            # Create a mask to guard memory operations against out-of-bounds accesses.
-            mask = offsets < n_elements
-            # Load x from DRAM, masking out any extra elements in case the input is not a multiple of the block size.
+            cols = i + tl.arange(0,BLOCK_SIZE)
+            mask = cols < dimensions[reduction_axis]
+            offsets = row_start + cols
             cur_block_vals = tl.load(x_ptr + offsets, mask=mask, other=-float('inf'))
+
             # Write Output to DRAM
-            tl.store(output_ptr + offsets, tl.exp(cur_block_vals - running_max) / running_denominator, mask=mask)
+            tl.store(output_ptr + offsets, tl.exp(cur_block_vals - global_max) / denominator, mask=mask)
 
     def safe_softmax_triton(x, dim=-1):
-
         # Similar to CUDA
         # 1. We need to preallocate the outputs
         #   - need to extract out the dim that we are reducing over
@@ -111,9 +115,67 @@ else:
         safe_softmax_triton_kernel[grid](x,output,dimensions,reduction_axis,n_elements,BLOCK_SIZE=H)
         return output
 
-    def online_softmax():
-        pass
+    @triton.jit
+    def online_softmax_triton_kernel(x_ptr,
+                                   output_ptr,
+                                   dimensions,
+                                   reduction_axis,
+                                   n_elements,
+                                   BLOCK_SIZE
+                                   ):
+        pid0 = tl.progam_id(axis=0)
+        pid1 = tl.progam_id(axis=1)
 
+        row_idx = pid0 * dimensions[1] + pid1
+        row_start = row_idx * dimensions[2]
+
+        # Running statistics to keep track off
+        prev_max = 0
+        running_max = float("-inf")
+        running_denominator = 0.0
+
+        for i in tl.range(0, dimensions[reduction_axis], BLOCK_SIZE):
+            block_start = i + row_start
+            cols = i + tl.arange(0, BLOCK_SIZE)
+            global_offsets = row_start + cols
+            # Create a mask to guard memory operations against out-of-bounds accesses.
+            mask = offsets < n_elements
+            # Load x from DRAM, masking out any extra elements in case the input is not a multiple of the block size.
+            cur_block_vals = tl.load(x_ptr + offsets, mask=mask, other=-float('inf'))
+
+            local_max = tl.max(cur_block_vals, axis=0)
+
+            # Then this should be sequential already no?
+            running_max = max(local_max,running_max)
+            running_denominator *= tl.exp(prev_max - running_max)
+            running_denominator += tl.sum(tl.exp(local_max - running_max), axis=0)
+            prev_max = running_max
+
+        # We will run the loop 1 more time to compute the actual softmaxed value
+        for i in tl.range(0, dimensions[reduction_axis], BLOCK_SIZE):
+            block_start = i+program_idx
+            offsets = tl.arange(block_start, block_start + BLOCK_SIZE)
+            # Create a mask to guard memory operations against out-of-bounds accesses.
+            mask = offsets < n_elements
+            # Load x from DRAM, masking out any extra elements in case the input is not a multiple of the block size.
+            cur_block_vals = tl.load(x_ptr + offsets, mask=mask, other=-float('inf'))
+            # Write Output to DRAM
+            tl.store(output_ptr + offsets, tl.exp(cur_block_vals - running_max) / running_denominator, mask=mask)
+
+    def online_softmax_triton(x, dim=-1):
+
+        output = torch.empty_like(x.shape[x:-1) # Let just assume we alway reduce over the last dimension first
+
+        assert x.device == DEVICE and y.device == DEVICE and output.device == DEVICE
+        n_elements = output.numel()
+        B,S,H = x.shape
+        #grid = lambda x : ()
+        grid = (B,S,) 
+        dimensions = x.shape
+        reduction_axis = 2
+
+        online_softmax_triton_kernel[grid](x,output,dimensions,reduction_axis,n_elements,BLOCK_SIZE=H)
+        return output
 
     # Compare Speed of softmax in pytorch and triton
     def scaled_dot_product_attn_flash():
